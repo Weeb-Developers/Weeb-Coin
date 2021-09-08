015725c9df58b28e90f4dd71a77d29ea38657d5f
ArtyG-10 <a.gourent@outlook.com>
2021-09-08 18:38:00 -0400

    implemented header with working sign in/out link

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,28 @@
-import React from "react"
-import PropTypes from "prop-types"
-class App extends React.Component {
+import React, { Component } from "react";
+import Header from './components/Header.js';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+class App extends Component {
   render () {
+    const {
+      logged_in,
+      current_user,
+      new_user_route,
+      sign_in_route,
+      sign_out_route
+    } = this.props
+
     return (
-      <React.Fragment>
-      </React.Fragment>
+      <Router>
+      <Header
+      logged_in={logged_in}
+      sign_in_route={sign_in_route}
+      sign_out_route={sign_out_route}
+      />
+      <h1>Hello {logged_in && current_user.username}</h1>
+      </Router>
     );
   }
 }
 
-export default App
+export default App;
