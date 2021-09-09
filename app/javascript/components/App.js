7a30f087a7578c5b2db415125818051f538f8c72
oddtomas <oddthomas@csu.fullerton.edu>
2021-09-09 10:58:00 -0700

    updates on read functionality

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 //Components
-import Header from './components/Header.js';
+import Header from "./components/Header.js";
 import Footer from "./components/Footer.js";
 
 //Pages
-import Home from './pages/Home'
+import Home from "./pages/Home";
 import Coin from "./pages/Coin.js";
 import AboutUs from "./pages/AboutUs.js";
 import CryptoInfo from "./pages/CryptoInfo.js";
@@ -14,39 +14,54 @@ import Portfolio from "./pages/Portfolio.js";
 import NotFound from "./pages/NotFound";
 
 class App extends Component {
-  constructor(){
-    super()
-      this.state = { 
-        coins: []
-      }
+  constructor() {
+    super();
+    this.state = {
+      coins: [],
+      portfolio: [],
+    };
   }
   componentDidMount() {
-    this.getCoins()
+    this.getCoins();
+    this.getPortfolio();
   }
 
   getCoins = () => {
     fetch("/coins")
-    .then(response => {
-      return response.json()
-    })
-    .then(payload => {
-      // set the state with the data from the backend into the empty array
-      this.setState({ coins: payload })
-    })
-    .catch(errors => {
-      console.log("index errors:", errors)
-    })
-  }
-  
-  render () {
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => {
+        // set the state with the data from the backend into the empty array
+        this.setState({ coins: payload });
+      })
+      .catch((errors) => {
+        console.log("index errors:", errors);
+      });
+  };
+  getPortfolio = () => {
+    fetch("/portfolios")
+      .then((response) => {
+        return response.json();
+      })
+      .then((payload) => {
+        // set the state with the data from the backend into the empty array
+        this.setState({ portfolio: payload });
+      })
+      .catch((errors) => {
+        console.log("index errors:", errors);
+      });
+  };
+
+  render() {
     const {
       logged_in,
       current_user,
       new_user_route,
       sign_in_route,
-      sign_out_route
-    } = this.props
-    
+      sign_out_route,
+    } = this.props;
+
     return (
       <Router>
         <Header
@@ -57,18 +72,48 @@ class App extends Component {
         <Switch>
           {/* <Route exact path='/' component={ <Home />}/> */}
 
-          <Route exact path='/' render={ (props) => 
-            <Home logged_in={logged_in} current_user={current_user} coins={ this.state.coins } />}/>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <Home
+                logged_in={logged_in}
+                current_user={current_user}
+                coins={this.state.coins}
+              />
+            )}
+          />
 
-          <Route path='/aboutus' component={ AboutUs }/>
-          <Route path='/coin' component={ Coin }/>
-          <Route path='/cryptoinfo' component={ CryptoInfo }/>
-          <Route path='/portfolio' component={ Portfolio }/>
-          <Route component={ NotFound }/>
+          <Route path="/aboutus" component={AboutUs} />
+          <Route
+            path="/coin/:id"
+            render={(props) => {
+              let id = props.match.params.id;
+              let coin = this.state.coins.find((coin) => coin.id === +id);
+              return <Coin coin={coin} logged_in={logged_in} />;
+            }}
+          />
+          <Route path="/cryptoinfo" component={CryptoInfo} />
+          <Route
+            path="/portfolio"
+            render={(props) => {
+              this.state.portfolio.filter(
+                (a) => a.user_id === this.props.current_user.id
+              );
+              return (
+                <Portfolio
+                  logged_in={logged_in}
+                  current_user={current_user}
+                  coins={this.state.coins}
+                  portfolio={this.state.portfolio}
+                />
+              );
+            }}
+          />
+          <Route component={NotFound} />
         </Switch>
-        <Footer 
-          logged_in
-        />
+        <Footer logged_in={logged_in} />
+        {console.log(this.state.portfolio && this.state.portfolio)}
       </Router>
     );
   }
--- app/javascript/components/pages/Coin.js
+++ app/javascript/components/pages/Coin.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { Card, Row, Col, CardText, CardTitle } from "reactstrap";
 
 class Coin extends Component {
-    render() {
-        return (
-            <div>
-                <h3>Coin page</h3>
-            </div>
-        );
-    }
-}
+  render() {
+    const { coin } = this.props;
+    return (
+      <Row key={coin.id}>
+        <Col sm="6">
+          <Card body>
+            <CardTitle tag="h5">Crypto {coin.id}</CardTitle>
+            <CardText>Name: {coin.name} </CardText>
+            <CardText>Symbol: {coin.symbol} </CardText>
+            <CardText>Price: {coin.price} </CardText>
+            <CardText>Last Updated: {coin.last_updated} </CardText>
+            <CardText>Market Cap: {coin.market_cap} </CardText>
+            <CardText>Total Supply: {coin.total_supply} </CardText>
+            <CardText>Volume in last 24H: {coin.volume_24h} </CardText>
 
+            <img src={coin.logo} width="300px" height="auto" />
+          </Card>
+        </Col>
+      </Row>
+    );
+  }
+}
 export default Coin;
