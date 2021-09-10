0a33c960fa5b69c368e07fb0bda2fa0a37e20920
charper95 <charper1995@gmail.com>
2021-09-10 14:15:09 -0700

    adds datetime format for home and show pages

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -3,6 +3,17 @@ import { Card, Row, Col, CardText, CardTitle } from "reactstrap";
 import { Link } from "react-router-dom";
 
 class Home extends Component {
+  constructor() {
+    super();
+  }
+
+  formatLastUpdated = (updateTime) => {
+    
+    let current_datetime = new Date(updateTime)
+    let formatted_date = current_datetime.getFullYear() + "-" + (current_datetime.getMonth() + 1) + "-" + current_datetime.getDate() + " " + current_datetime.getHours() + ":" + current_datetime.getMinutes() + ":" + current_datetime.getSeconds() 
+    return formatted_date
+  }
+
   render() {
     return (
       <section className="home-page">
@@ -22,7 +33,7 @@ class Home extends Component {
                         <img src={coin.logo} width="300px" height="auto" />
                         <CardText>{coin.name} </CardText>
                         <CardText>Price: ${coin.price} </CardText>
-                        <CardText>Last Updated: {coin.last_updated} </CardText>
+                        <CardText>Last Updated: {this.formatLastUpdated(coin.last_updated)} </CardText>
                       </Card>
                     </Link>
                   </Col>
