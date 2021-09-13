1c4c39426a759aed97231c84f3a6ac47c04f03cc
charper95 <charper1995@gmail.com>
2021-09-13 10:24:31 -0700

    working on modal

--- app/javascript/components/pages/Portfolio.js
+++ app/javascript/components/pages/Portfolio.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
-import { Card, Row, Col, CardText, CardTitle, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from "reactstrap";
+import { Card, Row, Col, CardText, CardTitle, Dropdown, DropdownToggle, DropdownMenu, DropdownItem, Button, Modal, ModalHeader, ModalBody, ModalFooter} from "reactstrap";
+// import { Modal, Button } from "react-bootstrap";
 import { Link, Redirect } from "react-router-dom";
 class Portfolio extends Component {
   constructor(){
     super();
     this.state = {
-      isOpen: false
+      isOpen: false,
+      modal: false,
     }
   }
   getTotalWorth = () => {
@@ -20,7 +22,21 @@ class Portfolio extends Component {
     let newOpenState = !this.state.isOpen
     this.setState({isOpen: newOpenState})};
 
-  handleClick = () => {this.state.submitted && <Redirect to='/apartmentindex'/>}
+  // handleClose = () => {
+  //   this.setState({show: false})
+  // }
+
+  // handleShow = () => {
+  //   this.setState({show: !this.state.show})
+  // }
+
+  toggleModal = () => {
+    let newOpenModal = !this.state.modal
+    this.setState({modal: newOpenModal})
+  }
+
+
+  // handleClick = () => {this.state.submitted && <Redirect to='/apartmentindex'/>}
 
   render() {
     console.log("coins", this.props.coins)
@@ -36,9 +52,21 @@ class Portfolio extends Component {
           </DropdownToggle>
           <DropdownMenu>
             {this.props.coins && this.props.coins.map((coin) => {
-            return <DropdownItem onClick={this.handleClick}>{coin.name}</DropdownItem>})}
+            return <DropdownItem onClick={this.toggleModal()}>{coin.name}</DropdownItem>})}
           </DropdownMenu>
         </Dropdown>
+
+        <Modal isOpen={this.state.modal} toggle={this.toggleModal()}>
+          <ModalHeader toggle={this.toggleModal()}>Modal title</ModalHeader>
+          <ModalBody>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" onClick={this.toggleModal()}>Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggleModal()}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
+
         <div>
           Current Crypto Curriences
           {this.props.portfolios.map((portfolio) =>{
