0cc902833a0a5ce31870e791e5a1bc78827b5cf6
ArtyG-10 <a.gourent@outlook.com>
2021-09-15 14:59:30 -0400

    fetch for api metadata complete

--- app/controllers/coins_controller.rb
+++ app/controllers/coins_controller.rb
@@ -1,14 +1,14 @@
 class CoinsController < ApplicationController
     def fetchAPI
         apiKey = ENV["API_KEY"]
-        apiData = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apiKey)
-        apiLogo = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=' + apiKey)
+        # apiData = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=' + apiKey)
+        apiLogo = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/info?CMC_PRO_API_KEY=' + apiKey+'&id=1,2,3,4,5,6,7,8,9,10')
         render json: apiLogo
-        coins = Coin.all
+        # coins = Coin.all
         # coins.create(name: apiData.data.name, symbol: apiData.data.symbol, price: apiData.data.quote.USD.price, volume_24h: apiData.data.quote.USD.volume_24h, market_cap: apiData.data.quote.USD.market_cap, last_updated: apiData.data.quote.USD.last_updated, total_supply: apiData.data.total_supply, logo: )
     end
 
-   
+
     def index
         coins = Coin.all
         render json: coins
--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   resources :portfolios
   resources :coins
   get '/api-data', to: 'coins#fetchAPI'
-
   devise_for :users
   get '*path', to: 'home#index', constraints: ->(request){ request.format.html? }
   root 'home#index'
-  
+
 end
