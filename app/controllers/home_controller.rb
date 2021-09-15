b690d4feb4b07fe1ef19563fd4073c06da9a1734
ArtyG-10 <a.gourent@outlook.com>
2021-09-15 13:18:44 -0400

    implemented hidden api key fetch

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
     def fetchAPI
-        data = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY=c16d4400-270c-4fe3-87c1-9292a3f03cfe')
+        apiKey = ENV["API_KEY"]
+        data = HTTParty.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC_PRO_API_KEY='+apiKey)
         render json: data
     end
-
 end
