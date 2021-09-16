8148202a41b3c4828be53f27cc7d2b9b50645ed0
ArtyG-10 <a.gourent@outlook.com>
2021-09-16 16:22:50 -0400

    limited decimal places to 2

--- app/javascript/components/pages/Home.js
+++ app/javascript/components/pages/Home.js
@@ -44,7 +44,7 @@ class Home extends Component {
                         <img src={`https://s2.coinmarketcap.com/static/img/coins/64x64/${coin.api_id}.png`}
                           height='64px' width='64px'/>
                         <CardText>{coin.name} </CardText>
-                        <CardText>Price: ${coin.price} </CardText>
+                        <CardText>Price: ${coin.price.toFixed(2)} </CardText>
                         <CardText>
                           Last Updated:{" "}
                           {this.formatLastUpdated(coin.last_updated)}{" "}
