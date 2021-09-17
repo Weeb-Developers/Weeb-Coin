21360785a4a6e84996b95bb08e034d4cb49ff601
oddtomas <oddthomas@csu.fullerton.edu>
2021-09-17 11:17:53 -0700

    added spacing for readability

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,7 +2,7 @@ class Coin < ApplicationRecord
     has_many :portfolios
     has_many :users, through: :portfolios
 
-    validates :name,:price,:symbol,:volume_24h,:market_cap,:total_supply,:last_updated,:api_id, presence: true
+    validates :name, :price, :symbol, :volume_24h, :market_cap, :total_supply, :last_updated, :api_id, presence: true
 
 
 end
